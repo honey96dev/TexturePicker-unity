38d8eaa96fa5d265e7f30cc746eb06936a20929b
sun1earth1moon1 <sun1earth1moon1@hotmail.com>
2019-03-13 09:03:15 -0700

    This is 1st version

---
+++ Assets/Unimgpicker/Samples/PickerController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kakera
+{
+    public class PickerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Unimgpicker imagePicker;
+        [SerializeField]
+        private UnimgpickerCamera imageCamera;
+
+        [SerializeField]
+        private MeshRenderer imageRenderer;
+
+        void Awake()
+        {
+            imagePicker.Completed += (string path) =>
+            {
+                StartCoroutine(LoadImage(path, imageRenderer));
+            };
+            imageCamera.Completed += (string path) =>
+            {
+                StartCoroutine(LoadImage(path, imageRenderer));
+            };
+        }
+
+        public void OnPressShowPicker()
+        {
+            imagePicker.Show("Select Image", "unimgpicker", 1024);
+        }
+
+        public void/*IEnumerator*/ OnPressShowCamera()
+        {
+            /*yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+            WebCamTexture webCamTexture = new WebCamTexture();
+            imageRenderer.material.mainTexture = webCamTexture;
+            webCamTexture.Play();
+            //imageCamera.Show("Capture Image", "unimgpicker", 1024);
+            */
+            int maxSize = 1024;
+            NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+            {
+                Debug.Log("Image path: " + path);
+                if (path != null)
+                {
+                    // Create a Texture2D from the captured image
+                    Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
+                    if (texture == null)
+                    {
+                        Debug.Log("Couldn't load texture from " + path);
+                        return;
+                    }
+
+                    // Assign texture to a temporary quad and destroy it after 5 seconds
+                    GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+                    quad.transform.forward = Camera.main.transform.forward;
+                    quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+
+                    Material material = quad.GetComponent<Renderer>().material;
+                    if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+                        material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+                    material.mainTexture = texture;
+
+                    Destroy(quad, 5f);
+
+                    // If a procedural texture is not destroyed manually, 
+                    // it will only be freed after a scene change
+                    Destroy(texture, 5f);
+                }
+            }, maxSize);
+
+            Debug.Log("Permission result: " + permission);
+        }
+
+        private IEnumerator LoadImage(string path, MeshRenderer output)
+        {
+            var url = "file://" + path;
+            var www = new WWW(url);
+            yield return www;
+
+            var texture = www.texture;
+            if (texture == null)
+            {
+                Debug.LogError("Failed to load texture url:" + url);
+            }
+
+            output.material.mainTexture = texture;
+        }
+    }
+}
